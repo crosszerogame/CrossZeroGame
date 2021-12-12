b0d9b708b17378b0c004ae73d6fe24a14a023c0a
Neosvet <neosvet333@gmail.com>
2021-12-12 15:25:56 +0300

    design settings fragment

--- app/src/main/java/ru/geekbrains/android2/crosszerogame/view/SettingsFragment.kt
+++ app/src/main/java/ru/geekbrains/android2/crosszerogame/view/SettingsFragment.kt
@@ -5,13 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import ru.geekbrains.android2.crosszerogame.R
+import ru.geekbrains.android2.crosszerogame.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+    private var binding: FragmentSettingsBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ) = FragmentSettingsBinding.inflate(inflater, container, false).also {
+        binding = it
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initFieldSize()
+        binding?.run {
+            btnFirst.isChecked = true
+            sbFieldsize.progress = 0
+        }
+    }
+
+    private fun initFieldSize() = binding?.run {
+        sbFieldsize.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                tvFieldsize.text = String.format(getString(R.string.field_size), i + 3, i + 3)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
