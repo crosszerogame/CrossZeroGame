a0fea5e5f108c574adcb1240c0bc8d2aa14963e6
Neosvet <neosvet333@gmail.com>
2021-12-30 15:00:06 +0300

    refactoring AiOpponent

--- app/src/main/java/ru/geekbrains/android2/crosszerogame/game/opponents/AiOpponent.kt
+++ app/src/main/java/ru/geekbrains/android2/crosszerogame/game/opponents/AiOpponent.kt
@@ -14,12 +14,16 @@ class AiOpponent(private val fieldSize: Int, private val isCross: Boolean) : Opp
     companion object {
         private const val DELAY_TIME: Long = 100
     }
+
     private val repository: GameRepository = GameRepositoryImpl()
     private val chipPlayer = if (isCross) Cell.ZERO else Cell.CROSS
     private val chipAI = if (isCross) Cell.CROSS else Cell.ZERO
     private val _state = MutableStateFlow<Opponent.State>(Opponent.State.Created)
     override val state: Flow<Opponent.State>
         get() = _state
+
+    private var playerStep = GameRepository.Step(0, 0, 0)
+    private var aiStep = GameRepository.Step(0, 0, 0)
 
     override fun preparing(): Flow<Player> = flow {
         repository.newGame(fieldSize)
@@ -40,7 +44,7 @@ class AiOpponent(private val fieldSize: Int, private val isCross: Boolean) : Opp
     }
 
     private suspend fun doMove() {
-        val move = aiTurn()
+        val move = aiMove()
         if (chipAI == Cell.CROSS)
             repository.pasteCross(move.first, move.second)
         else
@@ -51,79 +55,54 @@ class AiOpponent(private val fieldSize: Int, private val isCross: Boolean) : Opp
     override suspend fun sendBye() {
     }
 
-    // Функция Игоря Алексеенко, рефакторинг Юрия Тихомирова:
-// у противника выбирает элемент с максимальным количеством шагов до победы - stepMax,
-// при равных значениях stepMaxPlayer выбирает элемент с максимальным количеством проставленных фишек, входящих в победные комбинации - freedomWithFrMax,
-// при равных значениях freedomWithFrMax выбирает элемент с максимальным количество победных комбинаций - freedomMax
-// у себя выбирает элемент с максимальным количеством шагов до победы - stepMaxAi,
-// при равных значениях stepMaxAi выбирает элемент с максимальным количеством проставленных фишек, входящих в победные комбинации - freedomWithFrMax,
-// при равных значениях freedomWithFrMax выбирает элемент с максимальным количество победных комбинаций - freedomMax
-// для хода, если  stepMaxAi >= stepMaxPlayer, выбирается лучший элемент для себя, иначе выбирается лучший элемент для противника
-    private suspend fun aiTurn(): Pair<Int, Int> {
-        val map = repository.field
-        var xPlayer = 0
-        var yPlayer = 0
-        var xAi = 0
-        var yAi = 0
-        var stepMaxPlayer = 0
-        var stepMaxAi = 0
-        var freedomMax = 0
-        var freedomWithFrMax = 0
+    private suspend fun aiMove(): Pair<Int, Int> {
+        playerStep = GameRepository.Step(0, 0, 0)
+        aiStep = GameRepository.Step(0, 0, 0)
+        var playerMove = Pair(-1, -1)
+        var aiMove = Pair(-1, -1)
+        var move: Pair<Int, Int>
+
         for (y in 0 until fieldSize) {
             for (x in 0 until fieldSize) {
-                if (map[y][x] == Cell.EMPTY) {
-                    with(repository.calcStep(chipPlayer, x, y)) {
-                        if (step > stepMaxPlayer) {
-                            stepMaxPlayer = step
-                            freedomMax = freedom
-                            freedomWithFrMax = freedomWithFr
-                            xPlayer = x
-                            yPlayer = y
-                        } else if (step == stepMaxPlayer && freedomWithFr > freedomWithFrMax) {
-                            freedomWithFrMax = freedomWithFr
-                            freedomMax = freedom
-                            xPlayer = x
-                            yPlayer = y
-                        } else if (step == stepMaxPlayer && freedomWithFr == freedomWithFrMax && freedom > freedomMax) {
-                            freedomMax = freedom
-                            xPlayer = x
-                            yPlayer = y
-                        }
-                    }
-
-                    freedomMax = 0
-                    freedomWithFrMax = 0
-                    with(repository.calcStep(chipAI, x, y)) {
-                        if (step > stepMaxAi) {
-                            stepMaxAi = step
-                            freedomMax = freedom
-                            xAi = x
-                            yAi = y
-                        } else if (step == stepMaxAi && freedomWithFr > freedomWithFrMax) {
-                            freedomWithFrMax = freedomWithFr
-                            freedomMax = freedom
-                            xAi = x
-                            yAi = y
-                        } else if (step == stepMaxAi && freedomWithFr == freedomWithFrMax && freedom > freedomMax) {
-                            freedomMax = freedom
-                            xAi = x
-                            yAi = y
-                        }
-                    }
-
-                }
+                if (repository.field[y][x] != Cell.EMPTY)
+                    continue
+                move = calcMove(true, x, y)
+                if (move.first > -1)
+                    playerMove = move
+                move = calcMove(false, x, y)
+                if (move.first > -1)
+                    aiMove = move
             }
         }
 
-        val x: Int
-        val y: Int
-        if (stepMaxAi >= stepMaxPlayer) {
-            x = xAi
-            y = yAi
+        return if (aiStep.step >= playerStep.step)
+            aiMove
+        else
+            playerMove
+    }
+
+    private suspend fun calcMove(isPlayer: Boolean, x: Int, y: Int): Pair<Int, Int> {
+        val max: GameRepository.Step
+        val newMax: GameRepository.Step
+        if (isPlayer) {
+            max = playerStep
+            newMax = repository.calcStep(chipPlayer, x, y)
         } else {
-            x = xPlayer
-            y = yPlayer
+            max = aiStep
+            newMax = repository.calcStep(chipAI, x, y)
         }
-        return Pair(x, y)
+        with(newMax) {
+            if (step > max.step ||
+                (step == max.step && freedomWithFr > max.freedomWithFr) ||
+                (step == max.step && freedomWithFr == max.freedomWithFr && freedom > max.freedom)
+            ) {
+                if (isPlayer)
+                    playerStep = newMax
+                else
+                    aiStep = newMax
+                return Pair(x, y)
+            }
+        }
+        return Pair(-1, -1)
     }
 }
